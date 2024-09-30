aa192f3fb02b505f3aa3552b098e35378c9f14ae
Juanes2424 <55508909+Juanes2424@users.noreply.github.com>
2024-09-29 22:25:14 -0500

    Repos faltantes

---
+++ src/main/java/uniandes/edu/co/proyecto/controller/BodegaController.java
@@ -0,0 +1,43 @@
+package uniandes.edu.co.proyecto.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import uniandes.edu.co.proyecto.modelo.Bodega;
+import uniandes.edu.co.proyecto.repositorio.BodegaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class BodegaController {
+
+    @Autowired
+    private BodegaRepository bodegaRepository;
+
+    @PostMapping("/bodega/new")
+    public ResponseEntity<String> crearBodega(@RequestBody Bodega bodega) {
+
+        try {
+            bodegaRepository.insertarBodega(bodega.getNombre(), bodega.getTamano_metros2(),
+                    bodega.getSucursal().toString());
+            return new ResponseEntity<String>("Bodega creada exitosamente", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Error al crear bodega", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/bodega/{id}/delete")
+    public ResponseEntity<String> borrarSucursal(@PathVariable("id") String id) {
+        try {
+            bodegaRepository.borrarBodega(id);
+            return new ResponseEntity<String>("Exito borrar sucursal", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Error al borrar sucursal", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
